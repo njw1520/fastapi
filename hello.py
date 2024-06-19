c8af334229ecd8e4fdbad0db7c109c38189f99e1
Nate Webb <145239754+njw1520@users.noreply.github.com>
2024-06-18 21:02:55 -0400

    Added a few more endpoints for testing

--- hello.py
+++ hello.py
@@ -1,12 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Header, Response
 
 app = FastAPI()
 
-@app.get("/hi")
-def greet():
-    return "Hello? World?"
+@app.post("/hi")
+def greet(who:str = Header()):
+    return f"Hello {who}"
 
+@app.post("/agent")
+def get_agent(user_agent:str = Header()):
+    return user_agent
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("hello:app, reload=True")
+@app.get("/happy")
+def happy(status_code=200):
+    return ":)"
+
+@app.get("/header/{name}/{value}")
+def header(name: str, value: str, response:Response):
+    response.headers[name] = value
+    return "normal body"
